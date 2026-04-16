a1948b51c2b219e0d409a78c6d9a9c7e03a6b088
GoutamVaishnav <goutamvaishnav09@gmail.com>
2026-04-16 11:13:09 +0530

    same change

--- next.config.ts
+++ next.config.ts
@@ -7,20 +7,19 @@
 // };
 
 // export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     typedRoutes: true,
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/esbuild',
-        'node_modules/sharp',
-      ],
-    },
+  },
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/esbuild',
+      'node_modules/sharp',
+    ],
   },
 };
 
