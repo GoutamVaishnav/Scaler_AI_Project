f7e54200cc5cd1c4162f4f3f592c2b487a02ba6e
GoutamVaishnav <goutamvaishnav09@gmail.com>
2026-04-16 11:09:40 +0530

    next.config.ts change

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,27 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   experimental: {
+//     typedRoutes: true
+//   }
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    typedRoutes: true
-  }
+    typedRoutes: true,
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/esbuild',
+        'node_modules/sharp',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
